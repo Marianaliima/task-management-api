f136b2a5ee0e37b5ec961845f716efe417c9f083
marianaliima <marilimabrito13@gmail.com>
2024-07-21 18:59:21 -0300

    Initial commit

---
+++ src/task/task.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+
+    constructor(private readonly taskService: TaskService) {}
+    @Post()
+    create(@Body() task: TaskDto){
+        this.taskService.create(task)
+
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: string): TaskDto{
+      return  this.taskService.findById(id)
+    }
+    
+
+}
